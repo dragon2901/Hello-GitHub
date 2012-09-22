fd5334872d295c690dea2605f47a1663193f367c
tyhh <hongty.huynh@gmail.com>
2012-09-22 15:32:21 +0700

    First commit

---
+++ HelloGitHub/src/tyhh/learn/git/hello/Main.java
@@ -0,0 +1,18 @@
+package tyhh.learn.git.hello;
+
+/**
+ * Entry point class
+ * 
+ * @author tyhh
+ *
+ */
+public class Main {
+
+    /**
+     * Entry point method
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Hello GitHub!");
+    }
+}
